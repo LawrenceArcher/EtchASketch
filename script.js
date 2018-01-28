4f55ff7faeb244ada953c0f81a09cad3139ff599
Lawrence Archer <lawrence.archer@gmail.com>
2018-01-28 05:39:11 +0000

    Add div generating script

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let createGrid = () => {
 	var gridNumber = prompt("enter number between 1 and 64");
 	var container = document.querySelector(".container");
+	
+	
 	for (var i = 0; i < gridNumber;i++){
-		container.createElement("div");
+		var div = document.createElement("div");
+		div.classList.add("pixel");
+		container.appendChild(div);
+		console.log("run")
 	}
+	div.addEventListener("click", () =>{
+		div.backgroundColor = black;
+	})
 }
+createGrid();
